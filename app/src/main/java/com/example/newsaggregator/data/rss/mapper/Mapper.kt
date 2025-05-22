778688ae275b73024cd7a76683f674a3cb008fed
BaHe4ka666 <soulerfun@gmail.com>
2025-05-22 12:24:47 +0500

    almost done. Осталось только инъекцию зависимостей добавить

--- app/src/main/java/com/example/newsaggregator/data/rss/mapper/Mapper.kt
+++ app/src/main/java/com/example/newsaggregator/data/rss/mapper/Mapper.kt
@@ -3,7 +3,6 @@ package com.example.newsaggregator.data.rss.mapper
 import com.example.newsaggregator.data.rss.TextCleaner
 import com.example.newsaggregator.data.rss.network.model.RssDto
 import com.example.newsaggregator.domain.entity.NewsItem
-import javax.inject.Inject
 
 
 class Mapper {
